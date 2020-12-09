e411f1d82a89217d27e4cc3339aa0292ebed9a92
Ricart <ricarteusuo11@gmail.com>
2020-12-09 00:07:22 -0300

    env: adiciona conexão com postgres

---
+++ backend/app/database/postgres_connection.py
@@ -0,0 +1,13 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+# Carregando dados do ambiente virtual
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+PSQL_HOST = os.getenv("PSQL_HOST")
+PSQL_USER = os.getenv("PSQL_USER")
+PSQL_PASSWORD = os.getenv("PSQL_PASSWORD")
+
+conn_psql = psycopg2.connect(f"dbname={DB_NAME} user={PSQL_USER} password={PSQL_PASSWORD} host={PSQL_HOST}")
