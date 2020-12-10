ea34a1ae20b85d955139985471a0d690abd284d6
Ricart <ricarteusuo11@gmail.com>
2020-12-10 17:20:55 -0300

    fix: corrige notação de criação de cursor

--- backend/app/controllers/location_controller.py
+++ backend/app/controllers/location_controller.py
@@ -1,12 +1,12 @@
 from app import app
 from flask import jsonify
-from psycopg2.extras import RealDictCursor
+import psycopg2.extras
 from app.database.postgres_connection import conn_psql as psql
 
 
 @app.route("/location", methods=['GET'])
 def index():
-    cursor = psql.cursor(cursor_factory=RealDictCursor)
+    cursor = psql.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     statement = "SELECT * FROM localizacoes"
     cursor.execute(statement)
     result = cursor.fetchall()
