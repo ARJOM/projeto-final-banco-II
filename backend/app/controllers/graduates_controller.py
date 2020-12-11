5529ef7963f740e6c84aeb4c94ebea3738bdfabc
Ricart <ricarteusuo11@gmail.com>
2020-12-11 20:17:58 -0300

    feat: cria recurso de cadastro de egressos além disso corrige notação na listagem

--- backend/app/controllers/graduates_controller.py
+++ backend/app/controllers/graduates_controller.py
@@ -8,6 +8,22 @@ def list_graduate():
     colecao = mongo.egressos
     result = []
     for item in colecao.find():
-        item['__id'] = str(item['__id'])
+        item['_id'] = str(item['_id'])
         result.append(item)
+    print(result)
     return jsonify(result)
+
+
+@app.route("/graduate", methods=['POST'])
+def create_graduate():
+    colecao = mongo.egressos
+    data = request.json
+
+    # Verifica se já existe um egresso com a matricula informada
+    egressos = colecao.find({"matricula": data['matricula']})
+    for _ in egressos:
+        return {"msg": "Já existe um egresso registrado com a matrícula informada"}, 403
+
+    # Insere egresso
+    colecao.insert_one(data)
+    return {"msg": "Egresso registrado com sucesso"}
