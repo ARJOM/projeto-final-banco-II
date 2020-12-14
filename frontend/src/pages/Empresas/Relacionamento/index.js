cef0b0b517b8ded1bbdef5b9fd316dcbddeb007c
Glaymar <glaymar2010@live.com>
2020-12-14 19:50:15 -0300

    feat: implementa a criação de um novo funcionario a uma empresa

---
+++ frontend/src/pages/Empresas/Relacionamento/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import api from '../../../services/api';
+import { Link, useParams } from "react-router-dom";
+
+
+export default function Contratar() {
+    const [egressos, setEgressos] = useState([]);
+    const [empresa, setEmpresa] = useState('');
+    const [contratante, setContratante] = useState('');
+    const [contratado, setContratado] = useState('');
+    const [date, setdate] = useState('');
+
+    const { id } = useParams();
+
+    function handleContratar(e) {
+        e.preventDefault();
+
+        const data = {
+            egresso:contratado,
+            empresa:id,
+            data:date
+        };
+
+        api.post('/relation', data)
+            .then(res => console.log(res.data.msg))
+            .catch(err => console.log(err));
+    }
+
+    useEffect(() => {
+        api.get(`/company/${id}`)
+            .then(res => {
+                setEmpresa(res.data)
+            })
+
+        api.get("/graduate")
+            .then(res => setEgressos(res.data))
+            .catch(err => console.log(err));
+
+    }, [])
+
+    return (
+        <div>
+            <h2>Egressos para a {empresa.nome}</h2>
+            <p>Selecione o seu funcionario</p>
+            <form onSubmit={handleContratar}>
+                <select value={egressos._id} onChange={e => setContratado(e.target.value)}>
+                    {egressos.map(egresso => (
+                        <option key={egresso._id} value={egresso._id}>{egresso.nome}</option>
+                    ))}
+                </select>
+                <label>data: </label>
+                <input type="date" onChange={e => setdate(e.target.value)}/>
+                <button className="button" type="submit">Fazer Contrato</button>
+            </form>
+        </div>
+    )
+}
