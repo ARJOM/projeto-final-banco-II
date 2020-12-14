13d063f6250cd1edf1d0c782af73bed75bf64bd0
Ricart <ricarteusuo11@gmail.com>
2020-12-13 22:40:27 -0300

    feat: implementa cadastro de egresso

--- frontend/src/pages/Egressos/Create/index.js
+++ frontend/src/pages/Egressos/Create/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../../../services/api';
 
 export default function CreateEgresso(){
+    const [matricula, setMatricula] = useState("");
+    const [nome, setNome] = useState("");
+    const [curso, setCurso] = useState("");
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        const data = {matricula, nome, curso};
+
+        api.post('/graduate', data)
+            .then(res => console.log(res.data.msg))
+            .catch(err => console.log(err));
+    }
+
     return(
         <div>
             <h2>Cadastro de Egresso</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Nome: </label>
-                <input type="text"/>
+                <input type="text" onChange={e => setNome(e.target.value)}/>
                 <label>Matrícula: </label>
-                <input type="text"/>
+                <input type="text" onChange={e => setMatricula(e.target.value)}/>
                 <label>Curso</label>
-                <input type="text"/>
+                <input type="text" onChange={e => setCurso(e.target.value)}/>
 
-                <button type={"submit"}>Salvar</button>
+                <input type={"submit"} value={"Salvar"}/>
             </form>
         </div>
     )
